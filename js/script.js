61b91aa192dd91d4d79168ce49569fd1c405293d
Carlos Ramirez Rios <cramirez@b7-04.local>
2015-04-17 19:48:09 -0600

    terminado con JS

---
+++ js/script.js
@@ -0,0 +1,105 @@
+var AppValidando = {};
+  AppValidando.validacion = function(){
+
+    return {
+
+     validandoForm:function(e,n,m){
+                 var n = document.getElementById("nombre").value;
+                 var e = document.getElementById("email").value;
+                 var m = document.getElementById("mensaje").value;
+                 form = [];
+
+                    var nombre = /^\+$/;                   
+                    if( n === null || n.length === 0 || nombre.test(n) ) {
+                      alert("Pon bien tu nombre");
+                      return false;
+                     }
+                    var email = /^\+$/;;                   
+                    if( e === null || e.length === 0 || !(/^([a-zA-Z0-9_.-])+@(([a-zA-z0-9-])+.)+([a-zA-Z0-9-]{2,4})+$/.test(e))|| email.test(e) ) {
+                      alert("Pon bien tu email");
+                      return false;
+                    }                   
+                   var mensaje = /^\+$/;                   
+                    if( m === null || m.length === 0 || mensaje.test(m) ) {
+                          alert("Tu mensaje esta mal");
+                          return false;
+                     } else {
+                      form.push(n,e,m);
+                      document.getElementById("form").reset();
+                      alert("Tu mensaje ah sido enviado");
+                      console.log(form);
+                      return false;
+                     }
+                  }
+  }
+
+}();
+function informacion(a,e,m) {
+this.nombre = a;
+this.email = e;
+this.mensaje = m;
+}
+
+
+
+function ajaxRequest(){
+
+var mygetrequest =new XMLHttpRequest();
+mygetrequest.onreadystatechange = function(){
+  if (mygetrequest.readyState === 4 &&  mygetrequest.status == 200){
+    var jsonObj = JSON.parse(mygetrequest.responseText);
+
+    var content = "";
+    for (var i = 0; i < jsonObj.Proyectos.length; i++) {    
+        content += '<li class="img-proyect"><img class="img-proyect-ajax" src= "'+jsonObj.Proyectos[i].img+'"><span class="descript"><h2 class = text-descript>'+jsonObj.Proyectos[i].Nombre+'</h2></span><img class="mas-descript" src="'+jsonObj.Proyectos[i].imgMas+'"><p class="text-info-descript">'+jsonObj.Proyectos[i].informacion+'</p></li>'
+    };
+
+      document.getElementById("cont-proyect").innerHTML = content;
+  }
+}
+mygetrequest.open("GET", "js/proyectos.json", true);
+mygetrequest.send();
+};
+ajaxRequest();
+
+
+
+
+
+/*////////////////////////////////////// Validación /////////////////////////////////////////////////*/
+
+
+
+
+
+
+/*
+function validacion(val){
+      var elemento = document.getElementById(val);
+      if(elemento.checkValidity())
+          elemento.style.borderColor="Aqua";
+      else
+          elemento.style.borderColor = "red";
+    }
+    function buttonAction()
+    {
+      var nombreVal=document.getElementById('nombre').checkValidity();
+      var emailVal=document.getElementById('email').checkValidity();
+
+      if(nombreVal && emailVal){
+        alert("Su informacion fue enviada exitosamente");
+      } else{
+        alert("Por Favor revise sus campos.");
+      }   
+    }
+
+
+*/
+
+
+
+
+
+
+
+
