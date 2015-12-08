eb78fa724a325136f9d927a2e2015024b88c0c1a
Carlos Ramirez <cramirez@b7-06.local>
2015-12-08 15:11:46 -0600

    cambio tipografias

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
     $('.cont-li-header').toggleClass('cont');
   });
 });
+
+$(function() {
+	  $(".li-item-header-link").click(function() {
+        $("html, body").stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top - 0
+         }, 1500);
+        return false;
+    });
+});
